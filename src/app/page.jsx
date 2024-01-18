316c9a3ffc85f83a375ab9d825604a6be8daca92
Martin <martinbruun1995@gmail.com>
2024-01-18 13:33:50 +0100

    Add host script and update button functionality

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,34 @@
-'use client';
+'use client'
 
-import { NextUIProvider } from '@nextui-org/react';
-import { Button, buttonGroup } from '@nextui-org/react';
+import { NextUIProvider } from '@nextui-org/react'
+import { Button } from '@nextui-org/react'
+import getData from '@/lib/getData'
 
 export default function Home() {
+  async function tets() {
+    const data = await getData()
+    console.log(data)
+  }
+
+  async function getImage() {
+    const response = await fetch(
+      `https://graph.instagram.com/v18.0/17898781577791819?fields=id,media_type, media_url,username,timestamp&access_token=IGQWRQQUpPN0RZAcE9ON3U4M1gxRmtQanlQY1ROZAllOQkF2QXl4cVVIWkh2V0lueHlMRC1TUkpTck1mVmxacEw0d3FrTFJobjQ4RFM3SWZAOeWwxYXJzQS1ydFVsbnBTYWhPOHZAQb05QbGQxNVpDcEstaGNoeVVzT1kZD`
+    )
+
+    const data = await response.json()
+    console.log(data)
+  }
   return (
     <NextUIProvider>
       <div className="container">
-        <Button color="primary">Test</Button>
+        <Button onClick={tets} color="primary">
+          Test
+        </Button>
+
+        <Button onClick={getImage} color="primary">
+          image
+        </Button>
       </div>
     </NextUIProvider>
-  );
+  )
 }
