f26c20a4750adab7448cc2d15f3cfb3daebeb64d
Martin <martinbruun1995@gmail.com>
2024-01-22 11:28:38 +0100

    Add js-cookie dependency and implement token fetching and storing

--- src/lib/getData.jsx
+++ src/lib/getData.jsx
@@ -1,3 +1,11 @@
 'use server'
 
-export default async function getData() {}
+export default async function getData(TOKEN) {
+  const response = await fetch(
+    `https://graph.instagram.com/v18.0/me/media?access_token=${TOKEN}`
+  )
+
+  const data = await response.json()
+
+  return data
+}
