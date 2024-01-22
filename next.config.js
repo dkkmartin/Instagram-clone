76fbaef041cca25fde3393e9b704ad0f510b4648
Martin <martinbruun1995@gmail.com>
2024-01-22 18:25:12 +0100

    Add new SVG icons and update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['placehold.co'],
+    dangerouslyAllowSVG: true,
+  },
+}
 
 module.exports = nextConfig
