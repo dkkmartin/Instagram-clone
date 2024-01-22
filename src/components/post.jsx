2b787a53c6829c0e7c5ae79070a8da2ab71b5616
Sabircan <Sabircan@gmail.com>
2024-01-22 09:19:26 +0100

    removed post like,comment and date counter

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -39,11 +39,6 @@ export default function Post() {
                     </NavbarContent>
                 </Navbar>
             </article>
-            <article className="flex-column pl-10">
-                <p className="text-black"> 100 synes godt om</p>
-                <p className="text-gray-400">Vis alle 100 kommentarer</p>
-                <p className="text-gray-400">for 2 dag siden</p>
-            </article>
         </section >
     )
 }
