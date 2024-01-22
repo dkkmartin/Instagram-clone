f5044364ba26babd72ee0611800991d989f80163
Sabircan <Sabircan@gmail.com>
2024-01-22 10:38:35 +0100

    reinstall npm

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -6,12 +6,11 @@ import {
 export default function NavbarLink(props) {
     return (
         <NavbarItem className="flex-1">
-            <NavbarLink href="https://google.com/">
-                <Button className="text-black bg-transparent">
-                    <span className="material-symbols-outlined">
-                        {props.name}
-                    </span>
-                </Button></NavbarLink>
+            <Button className="text-black bg-transparent">
+                <span className="material-symbols-outlined">
+                    {props.name}
+                </span>
+            </Button>
         </NavbarItem>
     )
 }
