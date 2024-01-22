959785b71eb7c53f8b5765160e60d510b2234f6c
Martin <martinbruun1995@gmail.com>
2024-01-22 09:16:09 +0100

    Remove unnecessary code in getData.jsx

--- src/lib/getData.jsx
+++ src/lib/getData.jsx
@@ -1,11 +1,3 @@
 'use server'
 
-export default async function getData() {
-  const response = await fetch(
-    `https://graph.instagram.com/v18.0/me/media?access_token=${process.env.API_KEY}`
-  )
-
-  const data = await response.json()
-
-  return data
-}
+export default async function getData() {}
