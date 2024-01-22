178bd88c1e0c93ed1032700f6dfd4d39bb8eb4be
Sabircan <Sabircan@gmail.com>
2024-01-22 10:14:07 +0100

    test

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -6,11 +6,12 @@ import {
 export default function NavbarLink(props) {
     return (
         <NavbarItem className="flex-1">
-            <Button className="text-black bg-transparent">
-                <span className="material-symbols-outlined">
-                    {props.name}
-                </span>
-            </Button>
+            <NavbarLink href="https://google.com/">
+                <Button className="text-black bg-transparent">
+                    <span className="material-symbols-outlined">
+                        {props.name}
+                    </span>
+                </Button></NavbarLink>
         </NavbarItem>
     )
 }
