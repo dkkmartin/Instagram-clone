b85ded61b68eb2f29d0cc7535150e31f79c2d36e
AppleJuice1321 <rafaelgeisler6@gmail.com>
2024-01-23 11:02:32 +0100

    fix image review

--- src/components/UserPost/post.jsx
+++ src/components/UserPost/post.jsx
@@ -3,10 +3,12 @@
 import { Navbar, NavbarContent } from '@nextui-org/react'
 import PostsLinks from './post-links'
 import PostInfo from './post-info'
+import Image from 'next/image'
 
-export default function Post() {
+export default function Post({imageSrc}) {
   return (
     <section className="mb-20 mt-1">
+      <Image src={imageSrc} width={200} height={200}></Image>
       <div className="box-border h-80 w-80 mx-auto bg-black"></div>
       <article>
         <Navbar className="z-20">
