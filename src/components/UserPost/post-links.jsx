d0eab78633b7ffb8f9f8c2091fa845de61ad3acf
Martin <martinbruun1995@gmail.com>
2024-01-24 22:33:21 +0100

    Refactor post-links and remove unused components

--- src/components/UserPost/post-links.jsx
+++ src/components/UserPost/post-links.jsx
@@ -2,14 +2,14 @@
 import React from 'react'
 import Image from 'next/image'
 import { NavbarItem, Button, NavbarContent } from '@nextui-org/react'
-import LikeButton from '../likeButton'
+import LikeButton from './likeButton'
 
 export default function PostsLinks(props) {
   return (
     <NavbarContent className="flex">
       <NavbarItem className="flex-0">
         <Button isIconOnly color="none">
-          <LikeButton/>
+          <LikeButton />
         </Button>
       </NavbarItem>
       <NavbarItem className="flex-1">
