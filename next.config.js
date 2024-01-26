184c8f5515ec866c5c7d0d7447d7b8d90c82919a
Martin <martinbruun1995@gmail.com>
2024-01-26 09:36:24 +0100

    Add new domains to next.config.js and update Navbar component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['placehold.co', 'scontent-cph2-1.cdninstagram.com'],
+    domains: [
+      'placehold.co',
+      'scontent-cph2-1.cdninstagram.com',
+      'scontent.cdninstagram.com',
+    ],
     dangerouslyAllowSVG: true,
   },
 }
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -11,64 +11,69 @@ export default function PrimaryNavbar() {
   const pathname = usePathname()
 
   useEffect(() => {
-    if (pathname === '/login') setOnLoginRoute(true)
+    if (pathname === '/login') {
+      setOnLoginRoute(true)
+    } else {
+      setOnLoginRoute(false)
+    }
   }, [pathname])
 
   if (onLoginRoute) {
     return null
   } else {
-    ;<header className=" fixed z-40 bottom-0 w-full border-t">
-      <Navbar>
-        <NavbarContent
-          className="sm:flex gap-4 w-full justify-between"
-          justify=""
-        >
-          <Link href={'/'}>
-            <Button isIconOnly color="none">
-              <Image
-                src={'/MaterialSymbolsHouse.svg'}
-                width={30}
-                height={30}
-                alt=""
-              ></Image>
-            </Button>
-          </Link>
+    return (
+      <header className=" fixed z-40 bottom-0 w-full border-t">
+        <Navbar>
+          <NavbarContent
+            className="sm:flex gap-4 w-full justify-between"
+            justify=""
+          >
+            <Link href={'/'}>
+              <Button isIconOnly color="none">
+                <Image
+                  src={'/MaterialSymbolsHouse.svg'}
+                  width={30}
+                  height={30}
+                  alt=""
+                ></Image>
+              </Button>
+            </Link>
 
-          <Link href={'/'}>
-            <Button isIconOnly color="none">
-              <Image
-                src={'/MaterialSymbolsSearch.svg'}
-                width={30}
-                height={30}
-                alt=""
-              ></Image>
-            </Button>
-          </Link>
+            <Link href={'/'}>
+              <Button isIconOnly color="none">
+                <Image
+                  src={'/MaterialSymbolsSearch.svg'}
+                  width={30}
+                  height={30}
+                  alt=""
+                ></Image>
+              </Button>
+            </Link>
 
-          <Link href={'/'}>
-            <Button isIconOnly color="none">
-              <Image
-                src={'/MaterialSymbolsAddCircle.svg'}
-                width={30}
-                height={30}
-                alt=""
-              ></Image>
-            </Button>
-          </Link>
+            <Link href={'/'}>
+              <Button isIconOnly color="none">
+                <Image
+                  src={'/MaterialSymbolsAddCircle.svg'}
+                  width={30}
+                  height={30}
+                  alt=""
+                ></Image>
+              </Button>
+            </Link>
 
-          <Link href={'/profile'}>
-            <Button isIconOnly color="none">
-              <Image
-                src={'/MaterialSymbolsAccountCircle.svg'}
-                width={30}
-                height={30}
-                alt=""
-              ></Image>
-            </Button>
-          </Link>
-        </NavbarContent>
-      </Navbar>
-    </header>
-    return null
+            <Link href={'/profile'}>
+              <Button isIconOnly color="none">
+                <Image
+                  src={'/MaterialSymbolsAccountCircle.svg'}
+                  width={30}
+                  height={30}
+                  alt=""
+                ></Image>
+              </Button>
+            </Link>
+          </NavbarContent>
+        </Navbar>
+      </header>
+    )
   }
 }
