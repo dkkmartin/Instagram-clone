84254c29fd2a6ca82a54c2dc75571b33c7dbc89b
Sabircan <Sabircan@gmail.com>
2024-02-01 13:26:07 +0100

    saving on localstorage

--- src/components/UserPost/saveButton.jsx
+++ src/components/UserPost/saveButton.jsx
@@ -1,30 +1,39 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function SaveButton() {
-    const [isClicked, setIsClicked] = useState(false);
+    const [isClicked, setIsClicked] = useState(() => {
+        const savedState = window.localStorage.getItem('save_button');
+        return savedState ? JSON.parse(savedState) : true;
+    });
 
     function handleClick() {
-        setIsClicked(!isClicked)
+        setIsClicked(!isClicked);
     }
+
+    useEffect(() => {
+        window.localStorage.setItem('save_button', JSON.stringify(isClicked));
+    }, [isClicked]);
 
     return (
         <>
-            {
-                isClicked ? (
-                    <Image onClick={handleClick}
-                        src="/MaterialSymbolsBookmarkAdded.svg"
-                        alt=""
-                        width={30}
-                        height={30}
-                    ></Image>
-                ) : (<Image onClick={handleClick}
+            {isClicked ? (
+                <Image
+                    onClick={handleClick}
                     src="/MaterialSymbolsBookmarkAdd.svg"
-                    alt=""
+                    alt="Save the content"
                     width={30}
                     height={30}
-                ></Image>)
-            }
+                />
+            ) : (
+                <Image
+                    onClick={handleClick}
+                    src="/MaterialSymbolsBookmarkAdded.svg"
+                    alt="Unsave the content"
+                    width={30}
+                    height={30}
+                />
+            )}
         </>
-    )
+    );
 }
