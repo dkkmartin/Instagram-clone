2d0c60629b1defe01714cc720df394214ca881dc
Martin Bruun <47209447+dkkmartin@users.noreply.github.com>
2024-02-08 09:37:02 +0100

    Revert "Menu with logout btn"

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -2,9 +2,8 @@
 
 import Image from 'next/image'
 import { useData } from '@/stores/useMediaStore'
-import { Avatar, AvatarGroup, AvatarIcon, Button } from '@nextui-org/react'
+import { Avatar, AvatarGroup, AvatarIcon } from '@nextui-org/react'
 import Gallery from '@/components/Profile/gallery'
-import Link from 'next/link'
 
 export default function Profile() {
   const { data } = useData()
