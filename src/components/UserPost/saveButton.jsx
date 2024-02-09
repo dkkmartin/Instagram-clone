98f927ee38e6989f79f547675bd71077e225ab2e
Sabircan <Sabircan@gmail.com>
2024-02-09 09:17:41 +0100

    added logout button

--- src/components/UserPost/saveButton.jsx
+++ src/components/UserPost/saveButton.jsx
@@ -1,30 +1,39 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function SaveButton() {
-    const [isClicked, setIsClicked] = useState(false);
+    const [isSaved, setIsSaved] = useState(() => {
+        const savedState = window.localStorage.getItem('save_button');
+        return savedState ? JSON.parse(savedState) : true;
+    });
 
     function handleClick() {
-        setIsClicked(!isClicked)
+        setIsSaved(!isSaved);
     }
+
+    useEffect(() => {
+        window.localStorage.setItem('save_button', JSON.stringify(isSaved));
+    }, [isSaved]);
 
     return (
         <>
-            {
-                isClicked ? (
-                    <Image onClick={handleClick}
-                        src="/MaterialSymbolsBookmarkAdded.svg"
-                        alt=""
-                        width={30}
-                        height={30}
-                    ></Image>
-                ) : (<Image onClick={handleClick}
+            {isSaved ? (
+                <Image
+                    onClick={handleClick}
                     src="/MaterialSymbolsBookmarkAdd.svg"
-                    alt=""
+                    alt="Save the content"
                     width={30}
                     height={30}
-                ></Image>)
-            }
+                />
+            ) : (
+                <Image
+                    onClick={handleClick}
+                    src="/MaterialSymbolsBookmarkAdded.svg"
+                    alt="Unsave the content"
+                    width={30}
+                    height={30}
+                />
+            )}
         </>
-    )
+    );
 }
