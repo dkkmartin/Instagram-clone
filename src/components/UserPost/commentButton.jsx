a6f8608d02c3614c2f20941b848aa213339fe32f
Martin <martinbruun1995@gmail.com>
2024-02-15 13:56:06 +0100

    Refactor profile and user post components

---
+++ src/components/UserPost/commentButton.jsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Button } from '@nextui-org/react'
+import Image from 'next/image'
+
+export default function CommentButton({ postId, state, setState }) {
+  return (
+    <>
+      <Button onClick={() => setState(!state)} isIconOnly color="none">
+        <Image
+          src={'/PhChatTeardropDotsFill.svg'}
+          alt=""
+          width={30}
+          height={30}
+        ></Image>
+      </Button>
+    </>
+  )
+}
